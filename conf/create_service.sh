a23f64c392e29ad726129a0203406271d10183b3
Wojtek <wojciechss@wp.pl>
2017-07-11 22:15:59 +0200

    Update installation

--- conf/create_service.sh
+++ conf/create_service.sh
@@ -6,7 +6,9 @@ service_path=${args[1]}
 
 echo Create ${service_name} from ${service_path}
 
-sed "s:{DIR}:`pwd`:g;s:{USER}:`whoami`:g;s:{SERVICE_NAME}:${service_name}:g;s:{SERVICE_PATH}:${service_path}:g" ./conf/service > /etc/init.d/${service_name}
+sed "s:{DIR}:`pwd`:g;s:{USER}:`whoami`:g;s:{SERVICE_NAME}:${service_name}:g;s:{SERVICE_PATH}:${service_path}:g" ./conf/service > ./conf/${service_name}
 
-chmod 755 /etc/init.d/${service_name}
-update-rc.d ${service_name} defaults
+sudo mv ./conf/${service_name} /etc/init.d/${service_name}
+sudo chmod 755 /etc/init.d/${service_name}
+sudo chown root:root /etc/init.d/${service_name}
+sudo update-rc.d ${service_name} defaults
