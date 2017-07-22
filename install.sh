26b7ea890051bb272b83e1952546cee22b06f438
Wojtek <wojciechss@wp.pl>
2017-07-22 21:56:05 +0200

    Install analyzer

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 
 sudo ./conf/configure_python.sh
 
-#./conf/create_service.sh analyzer_service analyzer_service
+./conf/create_service.sh analyzer_service analyzer_service
 #./conf/create_service.sh controller_service controller/controller_service
 #./conf/create_service.sh mini_driver_service mini_driver/mini_driver_service
 #./conf/create_service.sh nano_service nano/nano_service
