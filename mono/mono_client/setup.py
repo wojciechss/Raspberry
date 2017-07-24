98b74cbaff8f50c2317010551226b3e741888e3d
Wojtek <wojciechss@wp.pl>
2017-07-24 21:45:28 +0200

    Remove mono service

--- mono/mono_client/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='mono_client',
-      version='0.1',
-      description='Mono client',
-      author='Wojciech Jozwiak',
-      license='MIT',
-      packages=['mono_client'],
-      zip_safe=False)
--- mono/mono_service/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gunicorn app:api --config gunicorn.conf
