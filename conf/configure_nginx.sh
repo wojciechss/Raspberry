42e9680a573c7ef2c4b1d3b4803859a6c1c68876
Wojtek <wojciechss@wp.pl>
2017-07-26 21:28:18 +0200

    Nginx http

--- conf/configure_nginx.sh
+++ conf/configure_nginx.sh
@@ -2,4 +2,5 @@
 
 echo 'Configure nginx'
 
-sed "s:{DIR}:`pwd`:g;" ./conf/nginx.conf > /etc/nginx/nginx.conf
+# sed "s:{DIR}:`pwd`:g;" ./conf/nginx.conf > /etc/nginx/nginx.conf
+sed "s:{DIR}:`pwd`:g;" ./conf/nginx_http.conf > /etc/nginx/nginx.conf
