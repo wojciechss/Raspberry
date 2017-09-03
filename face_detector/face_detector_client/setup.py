38de55f4713956ceb05c1df3f88ab7d125307886
Wojtek <wojciechss@wp.pl>
2017-09-03 10:20:44 +0200

    Fix setup face detector client

--- face_detector/face_detector_client/setup.py
+++ face_detector/face_detector_client/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='face_detector',
+setup(name='face_detector_client',
       version='0.1',
       description='Face detector client',
       author='Wojciech Jozwiak',
       license='MIT',
-      packages=['face_detector'],
+      packages=['face_detector_client'],
       zip_safe=False)
