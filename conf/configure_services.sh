df5a212f009298098ae60e1363e6f19699734c9f
Wojtek <wojciechss@wp.pl>
2018-07-29 19:56:32 +0200

    Configure services

--- conf/configure_services.sh
+++ conf/configure_services.sh
@@ -2,7 +2,4 @@
 
 echo 'Create services'
 
-./conf/create_service.sh controller_service controller/controller_service
-./conf/create_service.sh mini_driver_service mini_driver/mini_driver_service
-./conf/create_service.sh nano_service nano/nano_service
-./conf/create_service.sh camera_service camera/camera_service
+./conf/create_service.sh controller controller/controller_service
