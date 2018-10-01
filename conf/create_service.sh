4e7fd76f0d3193a402a9bf4559020c2f4f3dff0c
Wojtek <wojciechss@wp.pl>
2018-10-01 19:25:32 +0200

    Remove sed from service

--- conf/create_service.sh
+++ conf/create_service.sh
@@ -3,11 +3,9 @@
 args=("$@")
 service_name=${args[0]}
 
-echo Create ${service_name} from ${service_path}
+echo Create ${service_name}
 
-sed "s:{DIR}:`pwd`:g;s:{USER}:`whoami`:g;s:{SERVICE_NAME}:${service_name}:g;" ./conf/service > ./conf/${service_name}
-
-sudo mv ./conf/${service_name} /etc/init.d/${service_name}
+sudo cp ./conf/service /etc/init.d/${service_name}
 sudo chmod 755 /etc/init.d/${service_name}
 sudo chown root:root /etc/init.d/${service_name}
 sudo update-rc.d ${service_name} defaults
